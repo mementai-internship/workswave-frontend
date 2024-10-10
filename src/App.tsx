b0ad18c707b99ed14f9d855acc1fe1a125b63f78
davidktlee <rudxor567@naver.com>
2024-10-10 21:42:56 +0900

    feat: tanstack-query 및 devtools 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import Router from '@/routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'jotai';
 
 import './index.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    // 필요한 설정
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <Provider>
-        <h1 className="text-purple-50">workswave</h1>
-      </Provider>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <Provider>
+          <h1 className="text-purple-50">workswave</h1>
+          <Router />
+        </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
