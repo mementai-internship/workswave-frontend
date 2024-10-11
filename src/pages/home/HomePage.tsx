576cc4cac5e44bd9783bdd5670e3a745a0c7df8d
최주희 <choijuhee@choejuhuiui-MacBookAir.local>
2024-10-11 23:32:48 +0900

    feat:임시 컴포넌트 삭제

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,10 +1,5 @@
-import ContactSearchInput from '@/components/Common/ContactSearchInput';
 import React from 'react';
 
 export default function HomePage() {
-  return (
-    <div>
-      <ContactSearchInput />
-    </div>
-  );
+  return <div>HOME</div>;
 }
