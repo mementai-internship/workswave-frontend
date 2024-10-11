7f63bd4a8fbd42ab82e62c42f848f93d0f5ea5e7
“KwonKiBeom” <krshlee22@naver.com>
2024-10-11 14:18:04 +0900

    feat: 색상 tailwind 설정색상으로 변경 #1

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -15,8 +15,8 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="px-3 flex items-center justify-between text-white bg-neutral-800">
-      <div className="flex grow items-center justify-between pr-4 border-r border-neutral-700">
+    <header className="px-3 flex items-center justify-between text-white bg-gray-80">
+      <div className="flex grow items-center justify-between pr-4 border-r border-gray-50">
         <Link to="/">
           <span className="p-3">WorksWave</span>
         </Link>
@@ -27,10 +27,10 @@ export default function Header() {
           <HeaderProfile currentUser={currentUser} />
         ) : (
           <Link to="/login">
-            <button className="border border-slate-300 px-4 py-1 rounded mr-4">로그인</button>
+            <button className="border border-gray-10 px-4 py-1 rounded mr-4">로그인</button>
           </Link>
         )}
-        <div className="px-4 py-3.5 text-sm border-x border-neutral-700">
+        <div className="px-4 py-3.5 text-sm border-x border-gray-50">
           {/* 간편메뉴를 누르면 어떤일이 발생?? */}간편메뉴
         </div>
       </div>
