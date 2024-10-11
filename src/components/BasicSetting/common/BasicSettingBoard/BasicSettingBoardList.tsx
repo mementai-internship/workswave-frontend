48a3fd17aeff975d2b294141a52f2b42bede11f9
davidktlee <rudxor567@naver.com>
2024-10-11 21:10:53 +0900

    feat: 기본 설정 초기 설정

---
+++ src/components/BasicSetting/common/BasicSettingBoard/BasicSettingBoardList.tsx
@@ -0,0 +1,12 @@
+import BasicSettingBoardItem from '@/components/BasicSetting/Common/BasicSettingBoard/BasicSettingBoardItem';
+import { IBoardReponse } from '@/components/BasicSetting/types/types';
+
+export default function BasicSettingBoardList({ boardList }: { boardList: IBoardReponse[] }) {
+  return (
+    <div className="h-40 overflow-y-scroll border-t-2 p-10 flex flex-col gap-y-3">
+      {boardList.map(({ id, title, subTitle }) => (
+        <BasicSettingBoardItem key={id} id={id} title={title} subTitle={subTitle} />
+      ))}
+    </div>
+  );
+}
