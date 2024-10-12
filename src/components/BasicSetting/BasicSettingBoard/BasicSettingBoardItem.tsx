dd4ae6991bddd728ff8d0a8cc807eb2e8d998c26
davidktlee <rudxor567@naver.com>
2024-10-12 15:46:52 +0900

    fix: basic setting에서 사용하는 model 추가

--- src/components/BasicSetting/BasicSettingBoard/BasicSettingBoardItem.tsx
+++ src/components/BasicSetting/BasicSettingBoard/BasicSettingBoardItem.tsx
@@ -1,5 +1,5 @@
 import BasicSettingSubTitle from '@/components/Common/BasicSettingSubTitle';
-import { IBoardReponse } from '@/types/basicSettingtypes';
+import { IBoardReponse } from '@/models/basicSetting.model';
 import { Button } from '@radix-ui/themes';
 import { PiXBold } from 'react-icons/pi';
 
