ebabd377f57cb7003893cd14f75069b541e1ac37
Gene Young <jsbiggosoo@mement.ai>
2024-10-12 18:49:27 +0900

    feat: 회원 게시판/UI close #28

---
+++ src/components/UserBoard/UserBoardFilterBar/UserBoardFilterBar.tsx
@@ -0,0 +1,10 @@
+import UserBoardFilterBarDropdown from '@/components/UserBoard/UserBoardFilterBar/UserBoardFilterBarDropdown/UserBoardFilterBarDropdown';
+import React from 'react';
+
+export default function UserBoardFilterBar() {
+  return (
+    <div className="flex gap-4">
+      <UserBoardFilterBarDropdown />
+    </div>
+  );
+}
