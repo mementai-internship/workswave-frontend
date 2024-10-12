b550e1db695f5fca33fed1c8a5ab4bc5a26cd1a5
최주희 <choijuhee@choejuhuiui-MacBookAir.local>
2024-10-12 17:48:41 +0900

    feat: 근로관리 테이블 생성 #37

---
+++ src/models/workManagement/work.model.ts
@@ -0,0 +1,20 @@
+export type TLaborData = {
+  id: number;
+  branch: string;
+  name: string;
+  department: string;
+  workDate: number;
+  leaveDate: number;
+  regularDaysOff: number;
+  annualLeaveUsed: number;
+  unpaidLeaveUsed: number;
+  workFromHome: number;
+  holidayWork: number;
+  weekendWorkHours: number;
+  weekendWorkPay: number;
+  overtimeCount30min: number;
+  overtimeCount60min: number;
+  overtimeCount90min: number;
+  gender: number;
+  days: number;
+};
---
+++ src/pages/workManagement/WorkManagementPage.tsx
@@ -0,0 +1,10 @@
+import LaborTable from '@/components/WorkManagement/WorkTable';
+
+export default function LaborManagementPage() {
+  return (
+    <div>
+      근로관리1
+      <LaborTable />
+    </div>
+  );
+}
