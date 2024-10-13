2170f18578e6de0b09c4d2f242a991b54ddc93c4
memento-HYEMIN <hyemin@mement.ai>
2024-10-13 16:14:18 +0900

    refactor: 페이지 타이틀 SalaryRangeHeader에서 SalaryRangePage로 이동, Txt 컴포넌트 적용

--- src/components/BasicSetting/SalaryRange/SalaryRangeHeader.tsx
+++ src/components/BasicSetting/SalaryRange/SalaryRangeHeader.tsx
@@ -15,138 +15,131 @@ export default function SalaryRangeHeader({ register, onSubmit, salaryDetails }:
   const borderStyleDotted = `border-b border-dotted ${SALARY_RANGE_BORDER_COLOR}`;
 
   return (
-    <div className="w-full">
-      <div className="flex gap-6 title px-10 py-6">
-        <h3 className="text-xl">급여구간표</h3>
-        <p className="text-xl">뮤즈의원(강남점)</p>
-      </div>
+    <div className="flex w-full text-sm">
+      <table className={`border-t border-b ${SALARY_RANGE_BORDER_COLOR} border-l-0`}>
+        <tbody>
+          <tr>
+            <SalaryRangeTableTd bgColor="white" text="2024년 최저시급" styles="text-start px-2" />
+            <SalaryRangeTableTd bgColor="white" text={9680} />
+          </tr>
+          <tr>
+            <SalaryRangeTableTd
+              bgColor="white"
+              text="2024년 1개월 최저임금(기본급만)"
+              styles={`${headerTdCommonStyle} text-start`}
+            />
+            <SalaryRangeTableTd bgColor="white" text={2060740} />
+          </tr>
+        </tbody>
+      </table>
 
-      <div className="flex w-full text-sm">
-        <table className={`border-t border-b ${SALARY_RANGE_BORDER_COLOR} border-l-0`}>
+      <table className={`border ${SALARY_RANGE_BORDER_COLOR} flex-1`}>
+        <tbody>
+          <tr className={borderStyleDotted}>
+            <td></td>
+            <SalaryRangeTableTd bgColor="white" text="국민연금" />
+            <SalaryRangeTableTd bgColor="white" text="고용보험" />
+            <SalaryRangeTableTd bgColor="white" text="건강보험" />
+            <SalaryRangeTableTd bgColor="white" text="장기요양" />
+          </tr>
+          <tr>
+            <SalaryRangeTableTd bgColor="white" text="근로자" />
+            <SalaryRangeTableTd bgColor="white" text="4.5%" />
+            <SalaryRangeTableTd bgColor="white" text="0.9%" />
+            <SalaryRangeTableTd bgColor="white" text="3.545%" />
+            <SalaryRangeTableTd bgColor="white" text="12.95%" />
+          </tr>
+        </tbody>
+      </table>
+
+      <table className={`border-t border-b ${SALARY_RANGE_BORDER_COLOR} flex-1`}>
+        <tbody>
+          <tr className={`${borderStyleDotted} border-x-0 border-t-0`}>
+            <td></td>
+            <SalaryRangeTableTd bgColor="white" text="연금소득최저" />
+            <SalaryRangeTableTd bgColor="white" text="연금소득최대" />
+            <SalaryRangeTableTd bgColor="white" text="국민연금최대" />
+            <SalaryRangeTableTd bgColor="white" text="건보 하한선" />
+            <SalaryRangeTableTd bgColor="white" text="건보 상한선" />
+          </tr>
+          <tr>
+            <SalaryRangeTableTd bgColor="white" text="금액" />
+            <SalaryRangeTableTd bgColor="white" text={370000} />
+            <SalaryRangeTableTd bgColor="white" text={5900000} />
+            <SalaryRangeTableTd bgColor="white" text={265500} />
+            <SalaryRangeTableTd bgColor="white" text={9890} />
+            <SalaryRangeTableTd bgColor="white" text={4240710} />
+          </tr>
+        </tbody>
+      </table>
+
+      <form className="relative flex-1" onSubmit={onSubmit}>
+        <div
+          className={`caption absolute top-[-50px] left-[112px] inline-block bg-[#ffffff] rounded-md border ${SALARY_RANGE_BORDER_COLOR} px-4 py-2 shadow`}
+        >
+          <p>Enter Key 입력시 값이 계산됩니다.</p>
+          <div className="absolute right-[10px] bottom-[5px] -mb-4 ml-4 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[10px] border-t-[#dddddd]"></div>
+        </div>
+
+        <table className={`border-4 ${SALARY_RANGE_BORDER_COLOR} `}>
           <tbody>
-            <tr>
-              <SalaryRangeTableTd bgColor="white" text="2024년 최저시급" styles="text-start px-2" />
-              <SalaryRangeTableTd bgColor="white" text={9680} />
+            <tr className={`border-b ${SALARY_RANGE_BORDER_COLOR}`}>
+              <SalaryRangeTableTd
+                bgColor="white"
+                text="확인할 급여"
+                rowSpan={2}
+                borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
+                styles="px-2 text-lg"
+                isBoxShadow
+              />
+              <SalaryRangeTableTd
+                bgColor="white"
+                text="연봉"
+                borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
+                styles="min-w-28"
+              />
+              <SalaryRangeTableTd
+                bgColor="white"
+                text="월급여(세전)"
+                borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
+                styles="min-w-28"
+              />
+              <SalaryRangeTableTd
+                bgColor="white"
+                text="실수령액(식대비과세)"
+                borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
+                styles="min-w-32"
+              />
+              <SalaryRangeTableTd bgColor="white" text="실수령액(실수령액)" styles="min-w-32" />
             </tr>
             <tr>
               <SalaryRangeTableTd
                 bgColor="white"
-                text="2024년 1개월 최저임금(기본급만)"
-                styles={`${headerTdCommonStyle} text-start`}
+                text={salaryDetails.annualSalary}
+                borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
               />
-              <SalaryRangeTableTd bgColor="white" text={2060740} />
+              <td className="h-full bg-[#FFF7EC]">
+                <input
+                  {...register('inputSalary')}
+                  type="number"
+                  placeholder="입력해주세요"
+                  className="text-right bg-transparent focus:outline-none"
+                />
+              </td>
+              <SalaryRangeTableTd
+                bgColor="red"
+                text={salaryDetails.netPayWithMealAllowance}
+                borderStyle={`border-l ${SALARY_RANGE_BORDER_COLOR}`}
+              />
+              <SalaryRangeTableTd
+                bgColor="red"
+                text={salaryDetails.netPayWithoutMealAllowance}
+                borderStyle={`border-l ${SALARY_RANGE_BORDER_COLOR}`}
+              />
             </tr>
           </tbody>
         </table>
-
-        <table className={`border ${SALARY_RANGE_BORDER_COLOR} flex-1`}>
-          <tbody>
-            <tr className={borderStyleDotted}>
-              <td></td>
-              <SalaryRangeTableTd bgColor="white" text="국민연금" />
-              <SalaryRangeTableTd bgColor="white" text="고용보험" />
-              <SalaryRangeTableTd bgColor="white" text="건강보험" />
-              <SalaryRangeTableTd bgColor="white" text="장기요양" />
-            </tr>
-            <tr>
-              <SalaryRangeTableTd bgColor="white" text="근로자" />
-              <SalaryRangeTableTd bgColor="white" text="4.5%" />
-              <SalaryRangeTableTd bgColor="white" text="0.9%" />
-              <SalaryRangeTableTd bgColor="white" text="3.545%" />
-              <SalaryRangeTableTd bgColor="white" text="12.95%" />
-            </tr>
-          </tbody>
-        </table>
-
-        <table className={`border-t border-b ${SALARY_RANGE_BORDER_COLOR} flex-1`}>
-          <tbody>
-            <tr className={`${borderStyleDotted} border-x-0 border-t-0`}>
-              <td></td>
-              <SalaryRangeTableTd bgColor="white" text="연금소득최저" />
-              <SalaryRangeTableTd bgColor="white" text="연금소득최대" />
-              <SalaryRangeTableTd bgColor="white" text="국민연금최대" />
-              <SalaryRangeTableTd bgColor="white" text="건보 하한선" />
-              <SalaryRangeTableTd bgColor="white" text="건보 상한선" />
-            </tr>
-            <tr>
-              <SalaryRangeTableTd bgColor="white" text="금액" />
-              <SalaryRangeTableTd bgColor="white" text={370000} />
-              <SalaryRangeTableTd bgColor="white" text={5900000} />
-              <SalaryRangeTableTd bgColor="white" text={265500} />
-              <SalaryRangeTableTd bgColor="white" text={9890} />
-              <SalaryRangeTableTd bgColor="white" text={4240710} />
-            </tr>
-          </tbody>
-        </table>
-
-        <form className="relative flex-1" onSubmit={onSubmit}>
-          <div
-            className={`caption absolute top-[-50px] left-[112px] inline-block bg-[#ffffff] rounded-md border ${SALARY_RANGE_BORDER_COLOR} px-4 py-2 shadow`}
-          >
-            <p>Enter Key 입력시 값이 계산됩니다.</p>
-            <div className="absolute right-[10px] bottom-[5px] -mb-4 ml-4 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-t-[10px] border-t-[#dddddd]"></div>
-          </div>
-
-          <table className={`border-4 ${SALARY_RANGE_BORDER_COLOR} `}>
-            <tbody>
-              <tr className={`border-b ${SALARY_RANGE_BORDER_COLOR}`}>
-                <SalaryRangeTableTd
-                  bgColor="white"
-                  text="확인할 급여"
-                  rowSpan={2}
-                  borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
-                  styles="px-2 text-lg"
-                  isBoxShadow
-                />
-                <SalaryRangeTableTd
-                  bgColor="white"
-                  text="연봉"
-                  borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
-                  styles="min-w-28"
-                />
-                <SalaryRangeTableTd
-                  bgColor="white"
-                  text="월급여(세전)"
-                  borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
-                  styles="min-w-28"
-                />
-                <SalaryRangeTableTd
-                  bgColor="white"
-                  text="실수령액(식대비과세)"
-                  borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
-                  styles="min-w-32"
-                />
-                <SalaryRangeTableTd bgColor="white" text="실수령액(실수령액)" styles="min-w-32" />
-              </tr>
-              <tr>
-                <SalaryRangeTableTd
-                  bgColor="white"
-                  text={salaryDetails.annualSalary}
-                  borderStyle={`border-r ${SALARY_RANGE_BORDER_COLOR}`}
-                />
-                <td className="h-full bg-[#FFF7EC]">
-                  <input
-                    {...register('inputSalary')}
-                    type="number"
-                    placeholder="입력해주세요"
-                    className="text-right bg-transparent focus:outline-none"
-                  />
-                </td>
-                <SalaryRangeTableTd
-                  bgColor="red"
-                  text={salaryDetails.netPayWithMealAllowance}
-                  borderStyle={`border-l ${SALARY_RANGE_BORDER_COLOR}`}
-                />
-                <SalaryRangeTableTd
-                  bgColor="red"
-                  text={salaryDetails.netPayWithoutMealAllowance}
-                  borderStyle={`border-l ${SALARY_RANGE_BORDER_COLOR}`}
-                />
-              </tr>
-            </tbody>
-          </table>
-        </form>
-      </div>
+      </form>
     </div>
   );
 }
--- src/pages/basicSetting/SalaryRangePage.tsx
+++ src/pages/basicSetting/SalaryRangePage.tsx
@@ -1,5 +1,6 @@
 import SalaryRangeHeader from '@/components/BasicSetting/SalaryRange/SalaryRangeHeader';
 import SalaryRangeTable from '@/components/BasicSetting/SalaryRange/SalaryRangeTable';
+import { Txt } from '@/components/Common/Txt';
 import { useSalaryRange } from '@/hooks/useSalaryRange';
 
 export default function SalaryRangePage() {
@@ -16,6 +17,14 @@ export default function SalaryRangePage() {
     <div className="w-full p-4">
       <div className="max-w-[1722px] mx-auto w-full overflow-x-scroll border ${SALARY_RANGE_BORDER_COLOR}">
         <div className={`w-[1720px] bg-[#ffffff]`}>
+          <div className="flex gap-6 title px-10 py-6">
+            <Txt variant="subtitle1" color="text-black">
+              급여구간표
+            </Txt>
+            <Txt variant="subtitle1" color="text-black">
+              뮤즈의원(강남점)
+            </Txt>
+          </div>
           <SalaryRangeHeader
             salaryDetails={calculatedSalary.salaryDetails}
             onSubmit={handleSubmit(onSubmit)}
