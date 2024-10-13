7c9ecd84f9f9d37b3827866577ee3017ca9d50ec
lsjoon <lsj@lsjoon.local>
2024-10-13 20:39:52 +0900

    feat: 급여정산 페이지 테이블 및 계산기능 구현 #14

---
+++ src/pages/salarySettlement/SalarySettlementPage.tsx
@@ -0,0 +1,11 @@
+import SalaryTable from '@/components/SalarySettlement/SalaryTable';
+import React from 'react';
+
+export default function SalarySettlementPage() {
+  return (
+    <>
+      <h1>SalarySetPage</h1>
+      <SalaryTable />
+    </>
+  );
+}
