7d0ecf47179e8052b469605fcde067371054758d
“KwonKiBeom” <krshlee22@naver.com>
2024-10-14 13:53:17 +0900

    feat: 유저 권한에 따라 보여지는 내비게이션 UI 수정 #53

--- src/layout/Navigation/Navigation.tsx
+++ src/layout/Navigation/Navigation.tsx
@@ -1,13 +1,19 @@
 import { NAVIGATION_CONTENTS } from '@/layout/Navigation/constants';
 import { Link, useLocation } from 'react-router-dom';
 
+// 현재 로그인한 유저의 권한을 얻는 함수
+const getCurrentUserAuth = () => {
+  return 'STAFF';
+};
+
 export default function Navigation() {
+  const currentUserAuth = getCurrentUserAuth();
   const { pathname } = useLocation();
   const currentPath = pathname.split('/')[1];
 
   return (
     <nav className="flex mt-2 px-5 border-b border-gray-10">
-      {NAVIGATION_CONTENTS.map(({ path, content }) => (
+      {NAVIGATION_CONTENTS[currentUserAuth].map(({ path, content }) => (
         <Link
           to={path}
           key={content}
--- src/layout/Navigation/constants/index.ts
+++ src/layout/Navigation/constants/index.ts
@@ -1,4 +1,51 @@
-export const NAVIGATION_CONTENTS = [
+const MSO_NAVIGATION = [
+  {
+    path: '/member-management',
+    content: '회원관리',
+  },
+  {
+    path: '/board',
+    content: '회원게시판',
+  },
+  {
+    path: '/dayoff-management',
+    content: '연차관리',
+  },
+  {
+    path: '/ot-management',
+    content: 'O.T관리',
+  },
+  {
+    path: '/hr-management',
+    content: '인사관리',
+  },
+  {
+    path: '/work-management',
+    content: '근로관리',
+  },
+  {
+    path: '/document-management',
+    content: '문서관리',
+  },
+  {
+    path: '/salary-settlement',
+    content: '급여정산',
+  },
+  {
+    path: '/holiday-calander',
+    content: '휴무캘린더',
+  },
+  {
+    path: '/basic-setting',
+    content: '기본설정',
+  },
+  {
+    path: '/office-setting',
+    content: '지점설정',
+  },
+];
+
+const SUPER_MANAGER_NAVIGATION = [
   {
     path: '/member-management',
     content: '회원관리',
@@ -28,7 +75,7 @@ export const NAVIGATION_CONTENTS = [
     content: '급여정산',
   },
   {
-    path: '/dayoff-calander',
+    path: '/holiday-calander',
     content: '휴무캘린더',
   },
   {
@@ -40,3 +87,72 @@ export const NAVIGATION_CONTENTS = [
     content: '지점설정',
   },
 ];
+
+const PART_MANAGER_NAVIGATION = [
+  {
+    path: '/member-management',
+    content: '회원관리',
+  },
+  {
+    path: '/board',
+    content: '회원게시판',
+  },
+  {
+    path: '/dayoff-management',
+    content: '연차관리',
+  },
+  {
+    path: '/ot-management',
+    content: 'O.T관리',
+  },
+  {
+    path: '/hr-management',
+    content: '인사관리',
+  },
+  {
+    path: '/work-management',
+    content: '근로관리',
+  },
+  {
+    path: '/document-management',
+    content: '문서관리',
+  },
+  {
+    path: '/holiday-calander',
+    content: '휴무캘린더',
+  },
+  {
+    path: '/basic-setting',
+    content: '기본설정',
+  },
+];
+
+const STAFF_NAVIGATION = [
+  {
+    path: '/member-management',
+    content: '회원관리',
+  },
+  {
+    path: '/board',
+    content: '회원게시판',
+  },
+  {
+    path: '/dayoff-management',
+    content: '연차관리',
+  },
+  {
+    path: '/ot-management',
+    content: 'O.T관리',
+  },
+  {
+    path: '/holiday-calander',
+    content: '휴무캘린더',
+  },
+];
+
+export const NAVIGATION_CONTENTS = {
+  MSO: MSO_NAVIGATION,
+  SUPER_MANAGER: SUPER_MANAGER_NAVIGATION,
+  PART_MANAGER: PART_MANAGER_NAVIGATION,
+  STAFF: STAFF_NAVIGATION,
+};
