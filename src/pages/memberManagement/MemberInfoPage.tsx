680319ae43b778b082d77ae84545578b17627323
seoin <seoin1002@mement.ai>
2024-10-15 21:34:41 +0900

    feat: 회원관리 정보 Nav바 구현

--- src/pages/memberManagement/MemberInfoPage.tsx
+++ src/pages/memberManagement/MemberInfoPage.tsx
@@ -4,24 +4,61 @@ import ContractInfoBar from '@/components/MemberInfo/MemberInfoCommon/ContractIn
 import MemberInfoButtonBar from '@/components/MemberInfo/MemberInfoCommon/MemberInfoButtonBar';
 import SalaryContractTable from '@/components/MemberInfo/SalaryContractTable';
 import WorkContractTable from '@/components/MemberInfo/WorkContractTable';
+import { useState } from 'react';
 
 export default function MemberInfoPage() {
+  const [selectedTab, setSelectedTab] = useState<'info' | 'contract' | 'document'>('info');
+  function handleTabClick(tab: 'info' | 'contract' | 'document') {
+    setSelectedTab(tab);
+  }
+
   return (
     <div className="flex flex-col gap-2 w-full">
       <Title content="회원관리" />
-      <hr className="w-min-screen h-0.5 bg-gray-300" />
-      <MemberInfoButtonBar
-        leftButton={{ text: '증명서 신청' }}
-        rightButton={{ text: '신청 리스트' }}
-      />
-      <MemberInfoBasicTable />
-      <MemberInfoButtonBar
-        leftButton={{ text: '휴직신청' }}
-        rightButton={{ text: '신청 리스트' }}
-      />
-      <ContractInfoBar />
-      <WorkContractTable />
-      <SalaryContractTable />
+      <div>
+        <div className="flex gap-2">
+          <button
+            className={`${selectedTab === 'info' ? 'font-bold border-b-2 border-black' : ''} text-xl px-2 py-1`}
+            onClick={() => handleTabClick('info')}
+          >
+            정보
+          </button>
+          <button
+            className={`${selectedTab === 'contract' ? 'font-bold border-b-2 border-black' : ''} text-xl px-2 py-1`}
+            onClick={() => handleTabClick('contract')}
+          >
+            계약
+          </button>
+          <button
+            className={`${selectedTab === 'document' ? 'font-bold border-b-2 border-black' : ''} text-xl px-2 py-1`}
+            onClick={() => handleTabClick('document')}
+          >
+            문서·증명서
+          </button>
+        </div>
+        <hr className="w-min-screen h-0.5 bg-gray-300" />
+      </div>
+      {selectedTab === 'info' ? (
+        <>
+          <MemberInfoButtonBar
+            leftButton={{ text: '증명서 신청' }}
+            rightButton={{ text: '신청 리스트' }}
+          />
+          <MemberInfoBasicTable />
+        </>
+      ) : selectedTab === 'contract' ? (
+        <>
+          <MemberInfoButtonBar
+            leftButton={{ text: '휴직신청' }}
+            rightButton={{ text: '신청 리스트' }}
+          />
+          <ContractInfoBar />
+          <WorkContractTable />
+          <SalaryContractTable />
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
