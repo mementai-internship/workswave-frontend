324c278e97a94b4d2ab9c62aa846061d957fd8f5
“KwonKiBeom” <krshlee22@naver.com>
2024-10-16 00:56:34 +0900

    chore: axios 인스턴스 임시 주석처리 #104

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -1,4 +1,4 @@
-import { getAccessToken, getRefreshToken, removeTokens, setTokens } from '@/utils/tokenUtils';
+// import { getAccessToken, getRefreshToken, removeTokens, setTokens } from '@/utils/tokenUtils';
 import axios from 'axios';
 
 const axiosInstance = axios.create({
@@ -9,50 +9,50 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const accessToken = getAccessToken();
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const accessToken = getAccessToken();
+//     if (accessToken) {
+//       config.headers.Authorization = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const status = error.response?.status;
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   async (error) => {
+//     const status = error.response?.status;
 
-    if (status === 401) {
-      const refreshToken = getRefreshToken();
-      if (refreshToken) {
-        try {
-          const response = await axiosInstance.post('/refresh-token', {
-            refreshToken,
-          });
+//     if (status === 401) {
+//       const refreshToken = getRefreshToken();
+//       if (refreshToken) {
+//         try {
+//           const response = await axiosInstance.post('/refresh-token', {
+//             refreshToken,
+//           });
 
-          setTokens(response.data.accessToken, response.data.refreshToken);
+//           setTokens(response.data.refreshToken);
 
-          // 실패한 요청에 대해 Authorization 헤더 업데이트
-          error.config.headers['Authorization'] = `Bearer ${response.data.accessToken}`;
+//           // 실패한 요청에 대해 Authorization 헤더 업데이트
+//           error.config.headers['Authorization'] = `Bearer ${response.data.accessToken}`;
 
-          return axiosInstance.request(error.config);
-        } catch (refreshError) {
-          removeTokens();
-          window.location.href = '/';
-          return Promise.reject(refreshError);
-        }
-      }
-    } else if (status === 403) {
-      removeTokens();
-      window.location.href = '/login';
-      return Promise.reject(error);
-    }
+//           return axiosInstance.request(error.config);
+//         } catch (refreshError) {
+//           removeTokens();
+//           window.location.href = '/';
+//           return Promise.reject(refreshError);
+//         }
+//       }
+//     } else if (status === 403) {
+//       removeTokens();
+//       window.location.href = '/login';
+//       return Promise.reject(error);
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default axiosInstance;
