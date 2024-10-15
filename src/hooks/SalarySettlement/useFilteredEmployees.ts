9064ab68c6ebf1a60febb8cef9e228c7f0acf09f
lsjoon.dev <lsjoon.dev@mement.ai>
2024-10-15 11:53:45 +0900

    chore: 컨벤션에 맞게 수정

--- src/hooks/SalarySettlement/useFilteredEmployees.ts
+++ src/hooks/SalarySettlement/useFilteredEmployees.ts
@@ -1,18 +1,18 @@
 import { IEmployeeSalarySettlement } from '@/models/salarySettlement.model';
 import { useMemo } from 'react';
 
-export function useFilteredEmployees(
+export const useFilteredEmployees = (
   employees: IEmployeeSalarySettlement[],
   selectedRegion: string,
   selectedPart: string,
   selectedJob: string
-) {
+) => {
   return useMemo(() => {
-    return employees.filter((employee) => {
-      const regionMatch = selectedRegion ? employee.region === selectedRegion : true;
-      const partMatch = selectedPart ? employee.department === selectedPart : true;
-      const jobMatch = selectedJob ? employee.job === selectedJob : true;
-      return regionMatch && partMatch && jobMatch;
-    });
+    return employees.filter(
+      (employee) =>
+        (!selectedRegion || employee.region === selectedRegion) &&
+        (!selectedPart || employee.department === selectedPart) &&
+        (!selectedJob || employee.job === selectedJob)
+    );
   }, [employees, selectedRegion, selectedPart, selectedJob]);
-}
+};
