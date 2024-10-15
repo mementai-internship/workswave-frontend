5dcbf5c28541a78e68e53190b8749432c8ca1974
seoin <seoin1002@mement.ai>
2024-10-15 14:50:52 +0900

    refactor: 회원 정보 테이블 상수 분리

---
+++ src/constants/memberInfoTable.ts
@@ -0,0 +1,30 @@
+export const WEEK_DAY = [
+  {
+    id: 1,
+    name: '월',
+  },
+  {
+    id: 2,
+    name: '화',
+  },
+  {
+    id: 3,
+    name: '수',
+  },
+  {
+    id: 4,
+    name: '목',
+  },
+  {
+    id: 5,
+    name: '금',
+  },
+  {
+    id: 6,
+    name: '토',
+  },
+  {
+    id: 7,
+    name: '일',
+  },
+];
