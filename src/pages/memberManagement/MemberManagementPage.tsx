f109defb6f95916bb51e306dd6442902f24148a6
seoin <seoin1002@mement.ai>
2024-10-16 10:55:55 +0900

    feat: 회원관리 탭 구현

--- src/pages/memberManagement/MemberManagementPage.tsx
+++ src/pages/memberManagement/MemberManagementPage.tsx
@@ -7,11 +7,45 @@ import { useState } from 'react';
 export default function MemberManagementPage() {
   const [listPage, setListPage] = useState<number>(1);
   const itemsPerPage = 10;
+  const [selectedTab, setSelectedTab] = useState<string>('info');
+  function handleTabClick(tab: string) {
+    setSelectedTab(tab);
+  }
+
+  const tabList = [
+    {
+      name: '전체',
+      value: 'info',
+    },
+    {
+      name: '퇴사자',
+      value: 'contract',
+    },
+    {
+      name: '휴직자',
+      value: 'document',
+    },
+    {
+      name: '삭제회원',
+      value: 'delete',
+    },
+  ];
 
   return (
     <div className="flex flex-col gap-2 w-full">
       <Title content="회원관리" />
       <hr className="w-min-screen h-0.5 bg-gray-300" />
+      <div className="flex gap-2 border-b-2 border-gray-300">
+        {tabList.map((tab) => (
+          <button
+            key={tab.value}
+            className={`${selectedTab === tab.value ? 'font-bold border-b-2 border-black' : 'text-gray-50'} text-xl px-2 py-4 h-16`}
+            onClick={() => handleTabClick(tab.value)}
+          >
+            {tab.name}
+          </button>
+        ))}
+      </div>
       <MemberManagementFilterBar />
       <MemberManagementTable />
       <Pagination
