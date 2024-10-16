29d2f059fde25faeb000d618b8fe5a05387d2497
“KwonKiBeom” <krshlee22@naver.com>
2024-10-16 10:53:26 +0900

    feat: 레이아웃에서 전역상태 유저정보 사용 #104

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,26 +1,30 @@
 import HeaderProfile from '@/layout/Header/HeaderProfile';
 import { TUserAuthority } from '@/models/user.model';
+import { currentUserAtom } from '@/store/authAtoms';
+import { useAtom } from 'jotai';
 import { Link } from 'react-router-dom';
 
 export interface IHeader {
-  name: string;
-  role: TUserAuthority;
+  name?: string;
+  role?: TUserAuthority;
 }
 
-export default function Header({ name, role }: IHeader) {
+export default function Header() {
+  const [{ data }] = useAtom(currentUserAtom);
+
   return (
     <header className="px-3 flex items-center justify-between text-white bg-gray-80">
       <div className="flex grow items-center justify-between pr-4 border-r border-gray-50">
         <Link to="/">
           <span className="p-3">WorksWave</span>
         </Link>
-        {(role === 'MSO 최고권한' || role === '최고관리자') && (
+        {(data?.role === 'MSO 최고권한' || data?.role === '최고관리자') && (
           <span className="text-xs p-4">웍스웨이브 가이드</span>
         )}
       </div>
       <div className="flex items-center text-xs px-4">
-        {name && role ? (
-          <HeaderProfile name={name} role={role} />
+        {data ? (
+          <HeaderProfile name={data.name} role={data?.role} />
         ) : (
           <Link to="/login">
             <button className="border border-gray-10 px-4 py-1 rounded mr-4">로그인</button>
--- src/layout/Navigation/Navigation.tsx
+++ src/layout/Navigation/Navigation.tsx
@@ -1,4 +1,6 @@
 import { NAVIGATION_CONTENTS } from '@/layout/Navigation/constants';
+import { currentUserAtom } from '@/store/authAtoms';
+import { useAtom } from 'jotai';
 import { Link, useLocation } from 'react-router-dom';
 import { Fragment } from 'react/jsx-runtime';
 
@@ -11,52 +13,55 @@ type TNavigation = {
   }[];
 };
 
-export default function Navigation({ role }: { role: string }) {
+export default function Navigation() {
+  const [{ data }] = useAtom(currentUserAtom);
   const { pathname } = useLocation();
   const currentPath = pathname.split('/')[1];
 
   return (
     <>
-      <nav className="flex mt-2 px-5 border-b border-slate-200">
-        {role &&
-          NAVIGATION_CONTENTS[role].map(({ path, content }: TNavigation) => (
-            <Fragment key={path}>
-              <Link
-                to={path}
-                className={`py-4 grow flex items-center justify-center hover:cursor-pointer hover:bg-slate-200 
+      {data && (
+        <Fragment>
+          <nav className="flex mt-2 px-5 border-b border-slate-200">
+            {NAVIGATION_CONTENTS[String(data.role)].map(({ path, content }: TNavigation) => (
+              <Fragment key={path}>
+                <Link
+                  to={path}
+                  className={`py-4 grow flex items-center justify-center hover:cursor-pointer hover:bg-slate-200 
               ${path === `/${currentPath}` ? 'text-purple-50 border-b border-purple-50' : ''}`}
-              >
-                <li className="list-none">{content}</li>
-              </Link>
+                >
+                  <li className="list-none">{content}</li>
+                </Link>
+              </Fragment>
+            ))}
+          </nav>
+          {NAVIGATION_CONTENTS[String(data.role)]?.map(({ path, children }: TNavigation) => (
+            <Fragment key={path}>
+              {children && (
+                <div key={path} className="pl-6 gap-4 flex bg-slate-200 left-0 w-full">
+                  {children?.map(({ childPath, childContent }) => (
+                    <Fragment key={childPath}>
+                      {path === `/${currentPath}` && (
+                        <Link
+                          to={childPath}
+                          className={`py-3 text-xs flex items-center justify-center
+                ${childPath === pathname ? 'text-purple-50' : ''}`}
+                        >
+                          <li
+                            className={`list-none ${childPath === pathname ? 'border-b border-purple-50' : ''}`}
+                          >
+                            {childContent}
+                          </li>
+                        </Link>
+                      )}
+                    </Fragment>
+                  ))}
+                </div>
+              )}
             </Fragment>
           ))}
-      </nav>
-      {role &&
-        NAVIGATION_CONTENTS[role]?.map(({ path, children }: TNavigation) => (
-          <Fragment key={path}>
-            {children && (
-              <div key={path} className="pl-6 gap-4 flex bg-slate-200 left-0 w-full">
-                {children?.map(({ childPath, childContent }) => (
-                  <Fragment key={childPath}>
-                    {path === `/${currentPath}` && (
-                      <Link
-                        to={childPath}
-                        className={`py-3 text-xs flex items-center justify-center
-                ${childPath === pathname ? 'text-purple-50' : ''}`}
-                      >
-                        <li
-                          className={`list-none ${childPath === pathname ? 'border-b border-purple-50' : ''}`}
-                        >
-                          {childContent}
-                        </li>
-                      </Link>
-                    )}
-                  </Fragment>
-                ))}
-              </div>
-            )}
-          </Fragment>
-        ))}
+        </Fragment>
+      )}
     </>
   );
 }
