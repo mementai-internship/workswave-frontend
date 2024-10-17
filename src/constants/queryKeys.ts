8fcad56ad0c7ab905c7eab9f6005bd729bfffa68
seoin <seoin1002@mement.ai>
2024-10-17 13:31:04 +0900

    chore: 쿼리키 정리

---
+++ src/constants/queryKeys.ts
@@ -0,0 +1,7 @@
+export const QUERY_KEYS = {
+  //userManagement queryKeys
+  userManagementList: 'userList',
+  userManagementCurrentUser: 'currentUser',
+  userManagementDetail: 'userDetail',
+  userManagementResignedUsers: 'resignedUsers',
+};
