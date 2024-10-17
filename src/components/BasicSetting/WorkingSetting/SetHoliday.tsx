18f5b63d7c661b60a3e6ed42f0f77755c9a3326e
davidktlee <rudxor567@naver.com>
2024-10-17 21:37:59 +0900

    fix: 타입 에러 수정

--- src/components/BasicSetting/WorkingSetting/SetHoliday.tsx
+++ src/components/BasicSetting/WorkingSetting/SetHoliday.tsx
@@ -1,12 +1,13 @@
-import { IWorkingSettingBranchResponse } from '@/models/workingSetting.model';
 import { RadioGroup } from '@radix-ui/themes';
 import { UseFormWatch } from 'react-hook-form';
 import { UseFormRegister, UseFormSetValue } from 'react-hook-form';
 
+import { IWorkPolicies } from '@/models/work-policies';
+
 interface IPropsType {
-  register: UseFormRegister<IWorkingSettingBranchResponse>;
-  setValue: UseFormSetValue<IWorkingSettingBranchResponse>;
-  watch: UseFormWatch<IWorkingSettingBranchResponse>;
+  register: UseFormRegister<IWorkPolicies>;
+  setValue: UseFormSetValue<IWorkPolicies>;
+  watch: UseFormWatch<IWorkPolicies>;
 }
 export default function WorkingSettingHoliday({ register, setValue, watch }: IPropsType) {
   return (
@@ -17,10 +18,15 @@ export default function WorkingSettingHoliday({ register, setValue, watch }: IPr
         </label>
 
         <RadioGroup.Root
-          value={watch('do_holiday_work') ? 'true' : 'false'}
+          value={watch('holiday_work_policies.do_holiday_work') ? 'true' : 'false'}
           color="violet"
-          {...register('do_holiday_work')}
-          onValueChange={(newValue) => setValue('do_holiday_work', newValue === 'true')}
+          {...register('holiday_work_policies.do_holiday_work')}
+          onValueChange={(newValue) =>
+            setValue(
+              'default_allowance_policies.do_holiday_work' as keyof IWorkPolicies,
+              newValue === 'true'
+            )
+          }
         >
           <div className="flex gap-4">
             <RadioGroup.Item value="true">사용</RadioGroup.Item>
