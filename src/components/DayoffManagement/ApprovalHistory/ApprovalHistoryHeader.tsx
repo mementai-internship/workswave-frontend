bd9faa4fd70d9604ad637484bbc27215e9b2b2a1
Gene Young <jsbiggosoo@mement.ai>
2024-10-18 11:35:24 +0900

    feat: 연차전체승인내역UI

--- src/components/DayoffManagement/ApprovalHistory/ApprovalHistoryHeader.tsx
+++ src/components/DayoffManagement/ApprovalHistory/ApprovalHistoryHeader.tsx
@@ -5,29 +5,18 @@ import { Link } from 'react-router-dom';
 
 import TitleContainer from '@/components/Common/TitleContainer';
 
-export default function DayoffManagementHeader() {
+export default function ApprovalHistoryHeader() {
   return (
     <div>
-      <div className="flex  justify-between items-center ">
-        <TitleContainer content="연차관리">
+      <div className="flex items-center">
+        <TitleContainer content="전체승인내역">
           <div className="flex flex-row gap-2 items-center">
             <Button color="gray" variant="soft" radius="full" onClick={() => {}}>
               <PiGear />
-              <Link to="/dayoff-management/approval-history">전체승인내역</Link>
+              <Link to="/dayoff-management">연차관리</Link>
             </Button>
           </div>
         </TitleContainer>
-        <div>
-          <Button
-            variant="surface"
-            color="gray"
-            size="3"
-            className="font-bold text-black text-sm p-4 bg-gray-200 border-gray-10"
-            onClick={() => {}}
-          >
-            연차신청
-          </Button>
-        </div>
       </div>
       <hr className="w-min-screen h-0.5 bg-gray-300" />
     </div>
