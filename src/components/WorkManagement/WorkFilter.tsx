7d44adeaa400aa332b1f1df5c24836fd8ba335b2
lsjoon.dev <lsjoon.dev@mement.ai>
2024-10-19 17:05:25 +0900

    chore: 근로관리 - 필터 컴포넌트에서 월 선택 기능 삭제

--- src/components/WorkManagement/WorkFilter.tsx
+++ src/components/WorkManagement/WorkFilter.tsx
@@ -1,24 +1,10 @@
-import dayjs from 'dayjs';
-
-import { ChangeMonth } from '@/components/Common/ChangeMonth';
 import ContactSearchInput from '@/components/Common/ContactSearchInput';
 import WorkSelect from '@/components/WorkManagement/WorkSelect';
 
-interface IProps {
-  currentDate: dayjs.Dayjs;
-  setCurrentDate: React.Dispatch<React.SetStateAction<dayjs.Dayjs>>;
-}
-
-export default function WorkFilter({ currentDate, setCurrentDate }: IProps) {
-  const handleChangeMonth = (newDate: dayjs.Dayjs) => {
-    setCurrentDate(newDate);
-    // backend API 연결
-  };
-
+export default function WorkFilter() {
   return (
     <div className="flex justify-between py-5 border-t border-gray-200">
       <WorkSelect />
-      <ChangeMonth currMonth={currentDate} onChangeMonth={handleChangeMonth} />
       <ContactSearchInput />
     </div>
   );
