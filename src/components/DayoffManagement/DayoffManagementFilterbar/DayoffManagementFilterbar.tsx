8b32d80001f1c0c4fd50317030f9429474ff6469
Gene Young <jsbiggosoo@mement.ai>
2024-10-21 12:22:36 +0900

    feat: 연차관리 지점 필터 받아오기

--- src/components/DayoffManagement/DayoffManagementFilterbar/DayoffManagementFilterbar.tsx
+++ src/components/DayoffManagement/DayoffManagementFilterbar/DayoffManagementFilterbar.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import { useAtom } from 'jotai';
+import React, { useEffect } from 'react';
 
+import { getCurrentUser } from '@/apis/auth.api';
 import ContactSearchInput from '@/components/Common/ContactSearchInput';
 import SelectBox from '@/components/Common/Select';
 import WeekSelector from '@/components/DayoffManagement/DayoffManagementFilterbar/WeekSelector';
+import { useGetAllBranches } from '@/hooks/apis/useBranches';
+import { currentUserAtom } from '@/store/authAtoms';
 
 const dummyPart = [
   {
@@ -21,32 +25,46 @@ const dummyPart = [
     action: () => {},
   },
 ];
-
 export default function DayoffManagementFilterbar() {
+  const [currentUser] = useAtom(currentUserAtom);
+  const { data: branchData } = useGetAllBranches();
+
+  useEffect(() => {
+    getCurrentUser().then((res) => {
+      console.log(res);
+      console.log(branchData);
+    });
+  }, [currentUser]);
   return (
     <div className="flex gap-4 justify-between">
       <div>
-        <SelectBox
-          size="small"
-          title="지점 선택"
-          options={[
-            {
-              id: 1,
-              name: '뮤즈의원(강남점)',
+        {currentUser.data.role === 'MSO 최고권한' && (
+          <SelectBox
+            size="small"
+            title="지점 선택"
+            options={branchData?.map((branch) => ({
+              id: branch.id,
+              name: branch.name,
               action: () => {},
-            },
-            {
-              id: 2,
-              name: '뮤즈의원(수원인계점)',
-              action: () => {},
-            },
-          ]}
-        />
+            }))}
+          />
+        )}
+
         <SelectBox size="small" title="파트선택" options={dummyPart} />
         <SelectBox
           size="small"
           title="상태 변경"
           options={[
+            {
+              id: 0,
+              name: '전체',
+              action: () => {},
+            },
+            {
+              id: 3,
+              name: '대기',
+              action: () => {},
+            },
             {
               id: 1,
               name: '승인',
@@ -55,11 +73,6 @@ export default function DayoffManagementFilterbar() {
             {
               id: 2,
               name: '반려',
-              action: () => {},
-            },
-            {
-              id: 3,
-              name: '대기',
               action: () => {},
             },
           ]}
