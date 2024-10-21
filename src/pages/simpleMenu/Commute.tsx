3ba3a54fcea9c520d51628dcd13498bcd105aeab
juhee <woodyj9988@mement.ai>
2024-10-21 20:21:18 +0900

    feat:출근 등록 api 연결

--- src/pages/simpleMenu/Commute.tsx
+++ src/pages/simpleMenu/Commute.tsx
@@ -1,14 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import { usePostCommuteClockIn } from '@/hooks/apis/useCommute';
+
 export default function Commute() {
+  const { mutate: postCommuteClockIn } = usePostCommuteClockIn();
+
   return (
     <div className="flex flex-col gap-2 w-full">
-      <div className="text-center border border-slate-300 p-3 rounded-lg hover:bg-slate-100   cursor-pointer">
+      <div
+        className="text-center border border-slate-300 p-3 rounded-lg hover:bg-slate-100   cursor-pointer"
+        onClick={() => postCommuteClockIn()}
+      >
         출근
       </div>
       <div className="text-center border border-slate-300 p-3 rounded-lg hover:bg-slate-100   cursor-pointer">
         퇴근
       </div>
       <div className="text-center border border-slate-300 p-3 rounded-lg hover:bg-slate-100   cursor-pointer">
-        출/퇴근 내역
+        <Link to="/work-management/commute">출/퇴근 내역</Link>
       </div>
     </div>
   );
