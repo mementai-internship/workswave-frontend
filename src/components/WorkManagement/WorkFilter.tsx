e9fb8ea397ed3deb78ba9770b76ba779bf95d4c9
juhee <woodyj9988@mement.ai>
2024-10-21 14:33:07 +0900

    chore:select 수정, 페이지 이동시 설정 값  초기화

--- src/components/WorkManagement/WorkFilter.tsx
+++ src/components/WorkManagement/WorkFilter.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import ContactSearchInput from '@/components/Common/ContactSearchInput';
-import WorkSelect from '@/components/WorkManagement/WorkSelect';
+import WorkSelect, { TOptions } from '@/components/WorkManagement/WorkSelect';
+import { useGetAllBranches } from '@/hooks/apis/useBranches';
+import { useGetParts } from '@/hooks/apis/useParts';
 
 export default function WorkFilter() {
+  const [selectedBranch, setSelectedBranch] = useState<TOptions | null>(null);
+  const [selectedDepartment, setSelectedDepartment] = useState<TOptions | null>(null);
+
+  const { data: branches } = useGetAllBranches();
+  const { data: parts } = useGetParts(selectedBranch?.id || null);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (selectedBranch) {
+      setSelectedDepartment(null);
+    }
+  }, [selectedBranch]);
+
+  useEffect(() => {
+    setSelectedBranch(null);
+    setSelectedDepartment(null);
+  }, [location.pathname]);
+
+  const mapToOptions = (items: { id: number; name: string }[] | undefined) =>
+    items ? items.map((item) => ({ id: item.id, name: item.name })) : [];
+
   return (
     <div className="flex justify-between py-5 border-t border-gray-200">
-      <WorkSelect />
+      <div className="flex gap-3">
+        <WorkSelect
+          options={mapToOptions(branches)}
+          value={selectedBranch?.name}
+          onChange={(value: TOptions) => {
+            setSelectedBranch(value);
+          }}
+          defaultValue="지점 선택"
+        />
+        <WorkSelect
+          options={mapToOptions(parts)}
+          value={selectedDepartment?.name}
+          onChange={(value: TOptions) => {
+            setSelectedDepartment(value);
+          }}
+          defaultValue="파트 선택"
+        />
+      </div>
+
       <ContactSearchInput />
     </div>
   );
--- src/components/WorkManagement/WorkSelect.tsx
+++ src/components/WorkManagement/WorkSelect.tsx
@@ -1,80 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useLocation } from 'react-router-dom';
+import { Select } from '@radix-ui/themes';
 
-import SelectBox from '@/components/Common/Select';
-import { useGetAllBranches } from '@/hooks/apis/useBranches';
-import { useGetParts } from '@/hooks/apis/useParts';
-import { IPartsResponse } from '@/models/parts';
+export type TOptions = {
+  id: number;
+  name: string;
+};
+interface IWorkSelectProps {
+  options: TOptions[];
+  value: string | null;
+  onChange: (value: TOptions) => void;
+  defaultValue: string;
+}
 
-export default function WorkSelect() {
-  const { register, reset } = useForm();
-  const location = useLocation();
-
-  const [selectedBranch, setSelectedBranch] = useState<number | null>(null);
-  const [selectedDepartment, setSelectedDepartment] = useState<number | null>(null);
-
-  const { data: branches } = useGetAllBranches();
-  const { data: parts } = useGetParts(selectedBranch);
-
-  useEffect(() => {
-    setSelectedBranch(null);
-    setSelectedDepartment(null);
-    reset();
-  }, [location, reset]);
-
-  useEffect(() => {
-    setSelectedDepartment(null);
-  }, [selectedBranch]);
-
-  const branchOptions = branches
-    ? branches.map((branch) => ({
-        id: branch.id,
-        name: branch.name,
-        action: () => {
-          setSelectedBranch(branch.id);
-        },
-      }))
-    : [];
-
-  const partsOptions = parts
-    ? parts.map((part: IPartsResponse) => ({
-        id: part.id,
-        name: part.name,
-        action: () => {
-          setSelectedDepartment(part.id);
-        },
-      }))
-    : [];
-
-  const filteredBranchOptions = branchOptions.map((option) => ({
-    ...option,
-    selected: option.id === selectedBranch,
-  }));
-
-  const filteredPartsOptions = partsOptions.map((option) => ({
-    ...option,
-    selected: option.id === selectedDepartment,
-  }));
-
+export default function WorkSelect({ options, value, onChange, defaultValue }: IWorkSelectProps) {
   return (
-    <div className="flex gap-3">
-      <SelectBox
-        title="지점 선택"
-        name="branch"
-        options={filteredBranchOptions}
-        size="small"
-        border={true}
-        register={register}
-      />
-      <SelectBox
-        title="파트 선택"
-        name="position"
-        options={filteredPartsOptions}
-        size="small"
-        border={true}
-        register={register}
-      />
-    </div>
+    <Select.Root
+      size="2"
+      onValueChange={(selectedValue) => {
+        const selectedOption = options.find((option) => option.name === selectedValue);
+        if (selectedOption) {
+          onChange(selectedOption);
+        }
+      }}
+      value={value || defaultValue}
+    >
+      <Select.Trigger variant="surface" className="min-w-36">
+        {value || defaultValue}
+      </Select.Trigger>
+      <Select.Content>
+        {options.map((option) => (
+          <Select.Item key={option.id} value={option.name}>
+            {option.name}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
   );
 }
