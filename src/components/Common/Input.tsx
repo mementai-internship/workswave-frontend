ce4a6af80bb81a62be11e110cfa62bd7c962dc02
Gene Young <jsbiggosoo@mement.ai>
2024-10-21 11:01:20 +0900

    fix: 인풋 크기 변경 가능케함

--- src/components/Common/Input.tsx
+++ src/components/Common/Input.tsx
@@ -3,10 +3,15 @@ import React, { forwardRef } from 'react';
 
 interface IInputProps extends React.ComponentProps<typeof TextField.Root> {
   placeholder?: string;
+  size?: '1' | '2' | '3';
 }
 
-const Input = forwardRef<HTMLInputElement, IInputProps>(({ placeholder, ...rest }, ref) => {
-  return <TextField.Root ref={ref} radius="none" size="2" placeholder={placeholder} {...rest} />;
-});
+const Input = forwardRef<HTMLInputElement, IInputProps>(
+  ({ placeholder, size = '2', ...rest }, ref) => {
+    return (
+      <TextField.Root ref={ref} radius="none" size={size} placeholder={placeholder} {...rest} />
+    );
+  }
+);
 
 export default Input;
