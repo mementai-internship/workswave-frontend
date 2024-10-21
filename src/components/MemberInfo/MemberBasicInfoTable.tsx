0673d40dff9c36c64d9106af75c17a01b5771ccc
seoin <seoin1002@mement.ai>
2024-10-21 20:44:02 +0900

    feat: 다음 지도 API d.ts 생성

--- src/components/MemberInfo/MemberBasicInfoTable.tsx
+++ src/components/MemberInfo/MemberBasicInfoTable.tsx
@@ -13,47 +13,7 @@ import {
 import { useGetBranches } from '@/hooks/apis/useBranches';
 import { useGetParts } from '@/hooks/apis/useParts';
 import { useGetCurrentUserInfo, usePatchUser } from '@/hooks/apis/useUserManagement';
-
-type TZipCode = {
-  address: string;
-  addressEnglish: string;
-  addressType: string;
-  apartment: string;
-  autoJibunAddress: string;
-  autoJibunAddressEnglish: string;
-  autoRoadAddress: string;
-  autoRoadAddressEnglish: string;
-  bcode: string;
-  bname: string;
-  bname1: string;
-  bname1English: string;
-  bname2: string;
-  bname2English: string;
-  bnameEnglish: string;
-  buildingCode: string;
-  buildingName: string;
-  hname: string;
-  jibunAddress: string;
-  jibunAddressEnglish: string;
-  noSelected: string;
-  postcode: string;
-  postcode1: string;
-  postcode2: string;
-  postcodeSeq: string;
-  query: string;
-  roadAddress: string;
-  roadAddressEnglish: string;
-  roadname: string;
-  roadnameCode: string;
-  roadnameEnglish: string;
-  sido: string;
-  sidoEnglish: string;
-  sigungu: string;
-  sigunguCode: string;
-  sigunguEnglish: string;
-  userLanguageType: string;
-  zonecode: string;
-};
+import { TZipCode } from '@/models/zipCode.model';
 
 export default function MemberBasicInfoTable() {
   const { data: currentUserInfo } = useGetCurrentUserInfo();
@@ -136,7 +96,7 @@ export default function MemberBasicInfoTable() {
     new window.daum.Postcode({
       oncomplete: function (data: TZipCode) {
         basicInfoSetValue('address', data.address.toString());
-        console.log(data);
+        patchUser(basicInfoGetValues());
       },
       left: buttonRect.right + window.scrollX,
       top: buttonRect.bottom + window.scrollY,
