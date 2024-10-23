7275fc94a3de01a2fab774ce1a1077674911e80e
lsjoon.dev <lsjoon.dev@mement.ai>
2024-10-23 20:46:47 +0900

    refactor: 로그인 empty string 검증 로직 및 에러 시 alert 구현

--- src/apis/authAxiosInstance.ts
+++ src/apis/authAxiosInstance.ts
@@ -8,4 +8,28 @@ const authAxiosInstance = axios.create({
   withCredentials: true,
 });
 
+authAxiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const errStatus = error.response.status;
+    console.log('에러 발생', errStatus);
+
+    switch (errStatus) {
+      case 400:
+        alert(error.response.data);
+        break;
+
+      case 404:
+        alert(error.response.data);
+        break;
+
+      default:
+        break;
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default authAxiosInstance;
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -15,7 +15,12 @@ type TFormValues = {
 };
 
 export default function Login() {
-  const { handleSubmit, register } = useForm<TFormValues>();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm<TFormValues>();
   const [, setToken] = useAtom(userTokenAtom);
   const navigate = useNavigate();
 
@@ -23,18 +28,16 @@ export default function Login() {
   const handleLoginButtonClick = async (data: TFormValues) => {
     try {
       const { access_token } = await postLogin(data);
-      // 로그인 성공시 토큰값 저장
       setToken(access_token);
       navigate('/member-management');
     } catch (error) {
-      // 로그인 실패
       console.error(error, '로그인에 실패했습니다.');
     }
   };
 
   return (
-    <div className="flex-grow flex items-center justify-center">
-      <div className="flex flex-col w-full max-w-md gap-8 translate-y-[-18%]">
+    <div className="flex-grow h-80% flex items-center justify-center">
+      <div className="flex flex-col w-full max-w-md gap-10 translate-y-[-18%]">
         <div className="flex flex-col">
           <Txt className="flex items-center justify-center" variant="h1">
             WorksWave
@@ -43,30 +46,56 @@ export default function Login() {
             웍스웨이브웍스웨이브웍스웨이브
           </Txt>
         </div>
-        <form onSubmit={handleSubmit(handleLoginButtonClick)} className="flex flex-col gap-3">
-          <TextField.Root
-            className="flex-row-reverse items-center justify-center h-11 text-xs rounded-lg"
-            placeholder="이메일 주소"
-            {...register('email', {
-              required: true,
-            })}
-          >
-            <div className="pl-4 pr-2">
-              <AiOutlineMail size={16} />
-            </div>
-          </TextField.Root>
-          <TextField.Root
-            type="password"
-            className="flex-row-reverse items-center justify-center h-11 text-xs rounded-lg"
-            placeholder="비밀번호"
-            {...register('password', {
-              required: true,
-            })}
-          >
-            <div className="pl-4 pr-2">
-              <PiKey size={16} />
-            </div>
-          </TextField.Root>
+        <form onSubmit={handleSubmit(handleLoginButtonClick)} className="flex flex-col">
+          <div className="h-20">
+            <TextField.Root
+              className="flex-row-reverse items-center justify-center h-11 text-xs rounded-lg"
+              placeholder="이메일 주소"
+              {...register('email', {
+                required: '이메일을 입력해주세요',
+                validate: (value) => {
+                  if (value.trim() === '') {
+                    setValue('email', '');
+                    return '공백만 입력할 수 없습니다';
+                  }
+                  return true;
+                },
+              })}
+            >
+              <div className="pl-4 pr-2">
+                <AiOutlineMail size={16} />
+              </div>
+            </TextField.Root>
+            {errors.email && (
+              <p className="text-red-500 text-xs mt-2 mx-2">{errors.email.message}</p>
+            )}
+          </div>
+
+          <div className="h-20">
+            <TextField.Root
+              type="password"
+              className="flex-row-reverse items-center justify-center h-11 text-xs rounded-lg"
+              placeholder="비밀번호"
+              {...register('password', {
+                required: '비밀번호를 입력해주세요',
+                validate: (value) => {
+                  if (value.trim() === '') {
+                    setValue('password', '');
+                    return '공백만 입력할 수 없습니다';
+                  }
+                  return true;
+                },
+              })}
+            >
+              <div className="pl-4 pr-2">
+                <PiKey size={16} />
+              </div>
+            </TextField.Root>
+            {errors.password && (
+              <p className="text-red-500 text-xs mt-2 mx-2">{errors.password.message}</p>
+            )}
+          </div>
+
           <Button
             type="submit"
             variant="solid"
