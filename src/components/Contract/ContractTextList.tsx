85ec64e389dacdecf6f37759722854d4c48cf75d
Yeongjun Kim <kyjun@mement.ai>
2024-10-23 13:41:17 +0900

    feat: 계약서 작성 참고 텍스트 컴포넌트 추가

---
+++ src/components/Contract/ContractTextList.tsx
@@ -0,0 +1,57 @@
+interface IContractText_mock_type {
+  memberName: string;
+  branchName: string;
+  branchAddress: string;
+  exponentName: string;
+  exponentJourneyman: string;
+  exponentNameplate: string;
+  workerName: string;
+  workerSpot: string;
+  workerWork: string;
+  workerPhoneNumber: string;
+  workerEmail: string;
+  workerBrithDay: string;
+  engagedWork: string;
+  businessRegistrationNumber: string;
+  joinDay: string;
+  quitDay: string;
+  contractStartDay: string;
+  contractEndDay: string;
+}
+
+const ContractTextData: IContractText_mock_type = {
+  memberName: '의원명',
+  branchName: '지점명',
+  branchAddress: '지점주소',
+  exponentName: '대표자성명',
+  exponentJourneyman: '대표자직인',
+  exponentNameplate: '대표자명판',
+  workerName: '근로자성명',
+  workerSpot: '근로자직위',
+  workerWork: '근로자업무',
+  workerPhoneNumber: '근로자연락처',
+  workerEmail: '근로자이메일',
+  workerBrithDay: '근로자생년월일',
+  engagedWork: '종사업무',
+  businessRegistrationNumber: '사업자등록번호',
+  joinDay: '입사일',
+  quitDay: '퇴사일',
+  contractStartDay: '계약시작일',
+  contractEndDay: '계약종료일',
+};
+
+export default function ContractTextList({ documentName }) {
+  return (
+    <div className="flex flex-col w-1/5 gap-4 p-4 truncate bg-white border border-b-0 border-l-0 ">
+      <div className="flex flex-col text-xs">
+        <div>※ {documentName} 작성 시,</div>
+        <div className="truncate"> 복사하여 사용해주세요.</div>
+      </div>
+      <div className="flex flex-col gap-2">
+        {Object.entries(ContractTextData).map(([key, value]) => (
+          <div key={key} className="text-xs truncate text-slate-400">{`[${value}]`}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
