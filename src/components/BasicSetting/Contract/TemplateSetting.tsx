d41774806a877ee40516346def9cef55485400a7
yeongjun kim <kyjun@mement.ai>
2024-10-23 18:48:13 +0900

    feat: 문서 템플릿 설정 컴포넌트에 드롭다운 추가

--- src/components/BasicSetting/Contract/TemplateSetting.tsx
+++ src/components/BasicSetting/Contract/TemplateSetting.tsx
@@ -4,6 +4,7 @@ import CertificateTemplateList from '@/components/BasicSetting/Contract/Certific
 import ContractTemplateList from '@/components/BasicSetting/Contract/ContractTemplateList';
 import { DocumentMapData } from '@/components/BasicSetting/Contract/DocumentHeader';
 import DocumentTemplateList from '@/components/BasicSetting/Contract/DocumentTemplateList';
+import SelectBox from '@/components/Common/Select';
 
 interface ITemplateSettingProps {
   title: 'contract' | 'document' | 'certificate';
@@ -13,7 +14,7 @@ export function TemplateSetting({ title }: ITemplateSettingProps) {
   return (
     <div className="flex flex-col w-1/5 p-4 bg-white border">
       <div className="flex flex-col gap-1">
-        <div className="flex items-center justify-between mb-2">
+        <div className="flex items-center justify-between mb-1">
           <div className="font-medium">{DocumentMapData[title]} 템플릿 설정</div>
           <CiSettings
             size="20"
@@ -22,6 +23,30 @@ export function TemplateSetting({ title }: ITemplateSettingProps) {
             onClick={() => {}}
           />
         </div>
+        {title === 'document' && (
+          <SelectBox
+            title="지점"
+            size="2xSmall"
+            style="text-xs w-24"
+            options={[
+              {
+                id: 1,
+                name: '전체 문서',
+                action: () => {},
+              },
+              {
+                id: 2,
+                name: '발송된 문서',
+                action: () => {},
+              },
+              {
+                id: 3,
+                name: '마감된 문서',
+                action: () => {},
+              },
+            ]}
+          />
+        )}
 
         {title === 'contract' ? (
           <ContractTemplateList />
