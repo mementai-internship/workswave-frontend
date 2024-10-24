1c5127910e5656f93a167b5e5ce926236b1430d8
박요셉 <rkfnahs12e@mement.ai>
2024-10-24 20:48:54 +0900

    feat : 퇴근 api call 로직 추가

--- src/apis/commute.api.ts
+++ src/apis/commute.api.ts
@@ -4,11 +4,12 @@ export const commuteApi = {
   postCommuteClockIn: async () => {
     const result = await axiosInstance.post('/commutes/clock-in');
     console.log(result);
-    return result;
+    return result.data;
   },
   putCommuteClockOut: async (id: number) => {
+    console.log('id', id);
     const result = await axiosInstance.put(`/commutes/clock-out/${id}`);
     console.log(result);
-    return result;
+    return result.data;
   },
 };
